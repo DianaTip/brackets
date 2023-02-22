b73cd3ea371c5cc59c14760f75ce51dabc0e2a39
Diana <dianatipunova@gmail.com>
2023-02-22 09:50:39 +0300

    feat: add js file

--- src/index.js
+++ src/index.js
@@ -11,31 +11,40 @@ const brackets_pair = {
   ['7']: '7',
   ['8']: '8',
 };
-
+const res = str.split('');
 let stack = [];
-for (let i = 0; i < str.length; i ++)
-{var currentSymbol = str[i]
-/*if (currentSymbol = '|')
-   { if (stack[stack.length - 1] === '|')
-     {stack.pop()}
-     else 
-     {stack.push(currentSymbol)}}*/
+const br = bracketsConfig.flat();
+for (let i = 0; i < res.length; i ++)
+{var currentSymbol = res[i]
 
-   if (open_brackets.includes(currentSymbol))
-    {stack.push(currentSymbol)}
-    else {if (stack.length === 0)
-          {return false}
+  if (br.includes(currentSymbol))
+     {if (open_brackets.includes(currentSymbol))
+      {if ((currentSymbol === '|') || (currentSymbol === '7') || (currentSymbol === '8'))
+      {if (stack[stack.length-1] === currentSymbol)   
+          {stack.pop();}
+      else
+          {stack.push(currentSymbol);}
+       }
 
-    let topStack = stack[stack.length - 1];
-  
-      if (brackets_pair[currentSymbol] === topStack)
-        {stack.pop()}
-      else 
-      {return false}
-    }}
-
-    if (stack.length === 0)
-    {return true}
-    else 
+  else
+    {stack.push(currentSymbol) }}
+else 
+{if (stack.length === 0)
+{return false}
+else
+  {let topStack = stack[stack.length - 1];
+  if (brackets_pair[currentSymbol] === topStack)
+    {stack.pop()}
+  else 
     {return false}
+  }
+}        
+}
+else
+{return false}
+}
+if (stack.length === 0)
+{return true}
+else 
+{return false}
 }
